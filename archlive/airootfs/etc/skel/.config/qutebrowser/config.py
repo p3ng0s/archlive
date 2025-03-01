55920814779c8bf7e159774788ec46aab512f109
p4p1 <p4piiii@proton.me>
2025-03-01 08:13:30 +0000

    started migration to qutebrowser

---
+++ archlive/airootfs/etc/skel/.config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.load_autoconfig(True)
+c.auto_save.session = True
+c.content.blocking.method = "both"
+c.colors.webpage.darkmode.enabled = True
+c.content.cookies.accept = "no-3rdparty"
+c.tabs.title.format = "{audio}{index}: {current_url}"
+
