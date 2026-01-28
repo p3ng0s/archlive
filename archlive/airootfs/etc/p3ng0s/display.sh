9a72479caea29388658ae728672405ecf4199d0d
p4p1 <p4piiii@proton.me>
2026-01-28 10:44:58 +0100

    added display.sh edits

--- archlive/airootfs/etc/p3ng0s/display.sh
+++ archlive/airootfs/etc/p3ng0s/display.sh
@@ -11,14 +11,27 @@
 #  A p3ng0s script to handle external displays plugged in to the laptop.
 #
 
-
 function get_resolution() {
-	xrandr | awk -v d="$1" '$1==d && $2=="connected" {
-		split($3,a,"+"); print a[1]
-	}'
+    xrandr | awk -v d="$1" '
+    $1==d && $2=="connected" {flag=1; next}
+    flag && /^[[:space:]]+[0-9]+x[0-9]+/ {
+        gsub(/^[[:space:]]+/, "", $1)
+        split($1,res,"x")
+        w=res[1]; h=res[2]
+        area=w*h
+        if (area > max_area) {
+            max_area = area
+            max_res = $1
+        }
+    }
+    flag && $1=="" {flag=0}
+    END { print max_res }
+    '
 }
 
 mapfile -t FOUND_X_DISPLAYS < <(xrandr | awk '/ connected / {print $1}')
+
+echo ${FOUND_X_DISPLAYS[*]}
 
 # if there is an extra screen found other than built in
 if (( ${#FOUND_X_DISPLAYS[*]} == 2 )); then
@@ -29,9 +42,18 @@ if (( ${#FOUND_X_DISPLAYS[*]} == 2 )); then
 	# if the screen resolution matches my home monitor
 	if [[ "$res" == "2560x1440" ]]; then
 		# setting the correct positions
-		xrandr --output $OTHER --left-of $PRIMARY
+		xrandr --output $OTHER --auto --left-of $PRIMARY
 		feh --bg-fill $HOME/.wallpaper.png
 	fi
+	# if the screen resolution matches my other monitor
+	if [[ "$res" == "1920x1080" ]]; then
+		# setting the correct positions
+		xrandr --output $OTHER --auto --above $PRIMARY
+		feh --bg-fill $HOME/.wallpaper.png
+	fi
+else # no displays
+	xrandr --auto
+	feh --bg-fill $HOME/.wallpaper.png
 fi
 # the previous command might break the wallpaper don't forget to re-run
 # feh --bg-fill $HOME/.wallpaper.png
