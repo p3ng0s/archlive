1d473a736aa374fae1e4a02bee691d54ee73b8b2
p4p1 <p4piiii@proton.me>
2026-03-30 10:14:01 +0200

    tmux recovery

--- overlay/airootfs/etc/p3ng0s/tmuxer.sh
+++ overlay/airootfs/etc/p3ng0s/tmuxer.sh
@@ -8,22 +8,15 @@
 # (__)    (_)(__)  (__)
 #
 # Description:
-#  A quick script for tmux setup
+#  A script to connect to broken tmux sessions
 
-if [ -z "$(tmux ls | grep desktop)" ]; then
-	tmux new-session -d -s desktop
-	tmux rename-window -t desktop:0 'main'
-	tmux splitw -h -p 30 -t desktop:0.0
-	tmux splitw -v -p 30 -t desktop:0.0
-	tmux splitw -v -p 70 -t desktop:0.2
-	tmux send-keys -t desktop:0.2 'tmux clock -t desktop:0.2' Enter
-	tmux send-keys -t desktop:0.1 'newsboat' Enter
-	tmux send-keys -t desktop:0.0 'ranger' Enter
-	tmux new-window -t desktop
-	tmux rename-window -t desktop:1 'dev'
-	tmux send-keys -t desktop:1 'vim' Enter
-	tmux select-window -t desktop:0.0
-	tmux a -t desktop
+#mapfile -t SESSIONS < <(tmux ls -F '#{session_name} #{session_attached}' 2>/dev/null | awk '$2 == 0 {print $1}')
+SESSION_NAME=$(tmux ls -F '#{session_name} #{session_attached}' 2>/dev/null | awk '$2 == 0 {print $1; exit}')
+
+
+
+if [ ! -z "$SESSION_NAME" ]; then
+	tmux a -t "$SESSION_NAME"
 else
 	tmux
 fi
